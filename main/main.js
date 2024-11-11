f9488b406d702e6369bfed57a83c65f8f966f067
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-11-10 21:30:50 -0800

    canvas resizing well

--- main/main.js
+++ main/main.js
@@ -1,8 +1,37 @@
+const colors = {
+    "orange": "#FF8811",
+    "yellow": "#F4D06F",
+    "white": "#FFF8F0",
+    "lightBlue": "#9DD9D2",
+    "darkBlue": "#FF8811",
+}
+
 const canvas = document.querySelector("#canv");
 const ctx = canvas.getContext("2d");
 
-const width = canvas.width;
-const height = canvas.height;
+let width;
+let height;
 
-ctx.fillStyle = "#FF8811";
-ctx.fillRect(width/2, height/2, 50, 50);
+const redraw = () => {
+    ctx.fillStyle = colors.lightBlue;
+    ctx.fillRect(-width/2, -height/2, width, height);
+
+    ctx.fillStyle = colors.orange;
+    ctx.fillRect(0, 0, 50, 50);
+}
+
+const resizeCanvas = () => {
+    width = document.body.offsetWidth;
+    height = document.body.offsetHeight;
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    ctx.translate(width/2, height/2);
+    redraw();
+}
+
+
+window.addEventListener('resize', () => {
+    resizeCanvas();
+}, false);
+
+resizeCanvas();
