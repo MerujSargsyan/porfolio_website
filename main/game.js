97d7a08cfdd83fcf955743a0992171ca897e84fd
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-11-10 21:41:38 -0800

    modules up

---
+++ main/game.js
@@ -0,0 +1,10 @@
+'use strict'
+
+export const Player = {
+    "x": 0,
+    "y": 0,
+    "collision": {"w": 50, "h": 50},
+}
+
+export const parseInput = () => {
+};
--- main/main.js
+++ main/main.js
@@ -1,3 +1,7 @@
+'use strict'
+
+import {Player, parseInput} from "./game.js";
+
 const colors = {
     "orange": "#FF8811",
     "yellow": "#F4D06F",
@@ -17,7 +21,7 @@ const redraw = () => {
     ctx.fillRect(-width/2, -height/2, width, height);
 
     ctx.fillStyle = colors.orange;
-    ctx.fillRect(0, 0, 50, 50);
+    ctx.fillRect(Player.x, Player.y, Player.collision.x, Player.collision.y);
 }
 
 const resizeCanvas = () => {
@@ -26,12 +30,18 @@ const resizeCanvas = () => {
     ctx.canvas.width = width;
     ctx.canvas.height = height;
     ctx.translate(width/2, height/2);
-    redraw();
 }
 
+const update = () => {
+    parseInput();
+    redraw();
+}
 
 window.addEventListener('resize', () => {
     resizeCanvas();
 }, false);
 
-resizeCanvas();
+(function main() {
+    resizeCanvas();
+    setInterval(update, 1000 / 60);
+})();
